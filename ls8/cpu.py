66c37b920b8acc289888deb22e838d169772a8dd
Sydney Crumley <srsimps19@gmail.com>
2020-02-25 15:16:22 -0600

    finished day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,7 @@ import sys
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -16,33 +17,28 @@ class CPU:
         self.pc = 0
         self.halted = False
 
-    def load(self):
+    def load(self, file):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(file) as programFile:
+            for line in programFile:
+                splitLine = line.split("#")
+                numLine = splitLine[0].strip()
+                if numLine == '':
+                    continue
+                decVal = int(numLine, 2)
+                self.ram[address] = decVal
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
     
@@ -87,5 +83,8 @@ class CPU:
             elif ir == PRN:
                 print(self.reg[operand_a])
                 self.pc += 2
+            elif ir == MUL:
+                print(self.reg[operand_a] * self.reg[operand_b])
+                self.pc += 3
             else:
                 print("I did not understand that command.")
